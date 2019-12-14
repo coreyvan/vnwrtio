d854bdf6d4b219d3697f2ec74a09bc9321344529
Corey Van Woert <cvw@arenko.group>
2019-12-14 10:44:35 +0000

    Fix bootstrap overrides

--- src/js/custom.js
+++ src/js/custom.js
@@ -8,8 +8,8 @@ var items = navContainer.getElementsByClassName("nav-item");
 for (var i = 0; i < items.length; i++) {
     items[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+        current[0].className = current[0].className.replace(" active under-highlight", "");
+        this.className += " active under-highlight";
     });
 }
 
